fe02c0a36efb93080a51c4e97c40c62c494dcc74
Paweł Szram <pawel.szram@gmail.com>
2019-12-04 22:01:08 +0100

    feat(api): added unit tests

---
+++ ipstack.Geolocation.Tests/GeolocationTest.cs
@@ -0,0 +1,98 @@
+﻿namespace ipstack.Geolocation.Tests
+{
+    using System;
+    using System.Configuration;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class GeolocationTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            ConfigurationManager.AppSettings["ApiUrl"] = "http://api.ipstack.com/";
+            ConfigurationManager.AppSettings["ApiToken"] = "e71efcab54567e4deadbbe0f08c26ce3";
+        }
+
+        [Test]
+        public async Task ShouldGetSingleGeolocationResponse()
+        {
+            var geolocationService = TestHelper.GetGeolocationService();
+
+            var response = await geolocationService.GetGeolocation("127.0.0.1");
+
+            Assert.NotNull(response);
+        }
+
+        [Test]
+        public async Task ShouldThrowExceptionIfIpAddressDoesNotExists()
+        {
+            var geolocationService = TestHelper.GetGeolocationService();
+
+            var action = new Func<Task>(async () =>
+            {
+                await geolocationService.GetGeolocation(("1.1.1.1"));
+            });
+
+            await action.Should().ThrowAsync<Exception>();
+        }
+
+        [Test]
+        public async Task ShouldAddIpAddressToDatabase()
+        {
+            var geolocationService = TestHelper.GetGeolocationService();
+
+            await geolocationService.AddGeolocation("217.197.79.53");
+
+            var action = new Func<Task>(async () =>
+            {
+                await geolocationService.GetGeolocation("217.197.79.53");
+            });
+
+            action.Should().NotThrow();
+        }
+
+        [Test]
+        public async Task ShouldThrowExceptionIfIpAddressIsDuplicated()
+        {
+            var geolocationService = TestHelper.GetGeolocationService();
+
+            var action = new Func<Task>(async () =>
+            {
+                await geolocationService.AddGeolocation("127.0.0.1");
+            });
+
+            await action.Should().ThrowAsync<Exception>();
+        }
+
+        [Test]
+        public async Task ShoulDeleteIpAddressFromDatabase()
+        {
+            var geolocationService = TestHelper.GetGeolocationService();
+
+            await geolocationService.DeleteGeolocation("127.0.0.1");
+
+            var action = new Func<Task>(async () =>
+            {
+                await geolocationService.GetGeolocation("127.0.0.1");
+            });
+
+            await action.Should().ThrowAsync<Exception>();
+        }
+
+        [Test]
+        public async Task ShouldThrowExceptionIfDeleteAddressIpDoesNotExists()
+        {
+            var geolocationService = TestHelper.GetGeolocationService();
+
+            var action = new Func<Task>(async () =>
+            {
+                await geolocationService.DeleteGeolocation("217.197.79.53");
+            });
+
+            await action.Should().ThrowAsync<Exception>();
+        }
+    }
+}
---
+++ ipstack.Geolocation.Tests/TestHelper.cs
@@ -0,0 +1,40 @@
+﻿namespace ipstack.Geolocation.Tests
+{
+    using AutoMapper;
+    using DataAccess;
+    using DataAccess.Models;
+    using Services.Services;
+    using Services.Services.API;
+    using WebApi;
+
+    public static class TestHelper
+    {
+        public static IGeolocationService GetGeolocationService()
+        {
+            var mapper =
+                new MapperConfiguration(options => { options.AddProfile(new MappingsProfile()); }).CreateMapper();
+
+            var geolocationService = new GeolocationService(BuildDbContext(), mapper);
+
+            return geolocationService;
+        }
+
+        private static ApplicationDbContext BuildDbContext()
+        {
+            var builder = new ApplicationDbContext();
+
+            builder.Database.Delete();
+
+            var geolocation = new Geolocation
+            {
+                Ip = "127.0.0.1",
+                IsEu = true
+            };
+
+            builder.Geolocations.Add(geolocation);
+            builder.SaveChanges();
+
+            return builder;
+        }
+    }
+}
